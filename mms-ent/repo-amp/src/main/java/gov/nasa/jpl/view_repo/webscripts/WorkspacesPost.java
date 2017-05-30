c93a549c0258d6525d43b1e41fe6d560948b069f
Jason Han <jason.han@jpl.nasa.gov>
2017-05-30 11:00:40 -0700

    Return some json

--- mms-ent/repo-amp/src/main/java/gov/nasa/jpl/view_repo/webscripts/WorkspacesPost.java
+++ mms-ent/repo-amp/src/main/java/gov/nasa/jpl/view_repo/webscripts/WorkspacesPost.java
@@ -171,6 +171,7 @@ public class WorkspacesPost extends AbstractJavaWebScript {
         String workspaceName = null;
         String desc = null;
         String elasticId = null;
+        JSONObject branchJson = new JSONObject();
         Boolean isTag = false;
         String permission = "read";  // Default is public read permission
         EmsScriptNode finalWorkspace = null;
@@ -242,7 +243,7 @@ public class WorkspacesPost extends AbstractJavaWebScript {
                     dstWs.addAspect("ems:Workspace");
                     dstWs.setProperty("ems:workspace_name", newWorkspaceId);
 
-                    CommitUtil.sendBranch(projectId, srcJson, wsJson, elasticId, isTag, jsonObject != null ? jsonObject.optString("source") : null);
+                    branchJson = CommitUtil.sendBranch(projectId, srcJson, wsJson, elasticId, isTag, jsonObject != null ? jsonObject.optString("source") : null);
                     finalWorkspace = dstWs;
                 }
             }
@@ -278,6 +279,7 @@ public class WorkspacesPost extends AbstractJavaWebScript {
             wsJson.put(Sjm.MODIFIED, date);
             wsJson.put(Sjm.MODIFIER, user);
             elasticId = emsNodeUtil.insertSingleElastic(wsJson);
+            branchJson = wsJson;
             emsNodeUtil.updateRef(newWorkspaceId, workspaceName, elasticId, isTag);
         }
 
@@ -292,7 +294,7 @@ public class WorkspacesPost extends AbstractJavaWebScript {
             finalWorkspace.createOrUpdateProperty("ems:permission", permission);
         }
 
-        return wsJson;
+        return branchJson;
     }
 
 }
