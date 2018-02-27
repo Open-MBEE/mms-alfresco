b5d5f136897138fbc10c482599e0f2a36271a97c
Mann, Laura (393A-Affiliate) <Laura.Mann@jpl.nasa.gov>
2018-02-27 11:33:44 -0800

    wired the beans

--- mms-ent/repo-amp/src/main/java/gov/nasa/jpl/view_repo/webscripts/UserPreferencesGet.java
+++ mms-ent/repo-amp/src/main/java/gov/nasa/jpl/view_repo/webscripts/UserPreferencesGet.java
@@ -1,7 +1,95 @@
+/*******************************************************************************
+ * Copyright (c) <2013>, California Institute of Technology ("Caltech"). U.S. Government sponsorship
+ * acknowledged.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer. - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution. - Neither the name of Caltech nor its operating
+ * division, the Jet Propulsion Laboratory, nor the names of its contributors may be used to endorse
+ * or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+
 package gov.nasa.jpl.view_repo.webscripts;
 
+import gov.nasa.jpl.mbee.util.Timer;
+import gov.nasa.jpl.view_repo.util.CommitUtil;
+import gov.nasa.jpl.view_repo.util.LogUtil;
+import gov.nasa.jpl.view_repo.util.SerialJSONObject;
+import gov.nasa.jpl.view_repo.util.Sjm;
+import gov.nasa.jpl.view_repo.webscripts.util.ShareUtils;
+import org.alfresco.repo.model.Repository;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.service.ServiceRegistry;
+import org.alfresco.service.cmr.repository.StoreRef;
+import org.alfresco.service.cmr.search.ResultSet;
+import org.alfresco.service.cmr.search.ResultSetRow;
+import org.alfresco.service.cmr.search.SearchService;
+import org.alfresco.service.cmr.site.SiteInfo;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.extensions.webscripts.Cache;
+import org.springframework.extensions.webscripts.Status;
+import org.springframework.extensions.webscripts.WebScriptRequest;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * Created by lauram on 2/27/18.
+ *
+ * @author han
  */
-public class UserPreferencesGet {
+public class UserPreferencesGet extends AbstractJavaWebScript {
+    static Logger logger = Logger.getLogger(OrgPost.class);
+
+    public UserPreferencesGet() {
+        super();
+    }
+
+    public UserPreferencesGet(Repository repositoryHelper, ServiceRegistry registry) {
+        super(repositoryHelper, registry);
+    }
+
+    /**
+     * Webscript entry point
+     */
+    @Override protected Map<String, Object> executeImpl(WebScriptRequest req, Status status, Cache cache) {
+        OrgPost instance = new OrgPost(repository, getServices());
+        return instance.executeImplImpl(req, status, cache);
+    }
+
+    @Override protected Map<String, Object> executeImplImpl(WebScriptRequest req, Status status, Cache cache) {
+        String user = AuthenticationUtil.getFullyAuthenticatedUser();
+        String username = req.getServiceMatch().getTemplateVars().get(USERNAME);
+
+
+        return null;
+    }
+
+    /**
+     * Validate the request and check some permissions
+     */
+    @Override protected boolean validateRequest(WebScriptRequest req, Status status) {
+        return checkRequestContent(req);
+    }
+
 }
