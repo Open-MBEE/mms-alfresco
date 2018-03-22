f708c9af9293a75d1d1d6c4fab0ca666c7f5e19a
Jason Han <jason.han@jpl.nasa.gov>
2018-03-21 17:47:34 -0700

    Adding version comparision

--- mms-ent/repo-amp/src/main/java/gov/nasa/jpl/view_repo/actions/MigrationRunner.java
+++ mms-ent/repo-amp/src/main/java/gov/nasa/jpl/view_repo/actions/MigrationRunner.java
@@ -1,7 +1,6 @@
 package gov.nasa.jpl.view_repo.actions;
 
 import gov.nasa.jpl.view_repo.util.EmsConfig;
-import org.alfresco.repo.model.Repository;
 import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.cmr.module.ModuleDetails;
 import org.alfresco.service.cmr.module.ModuleService;
@@ -9,7 +8,13 @@ import org.alfresco.service.namespace.NamespaceService;
 import org.alfresco.service.namespace.QName;
 import org.apache.log4j.Logger;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Jason Han
@@ -20,6 +25,17 @@ public class MigrationRunner {
 
     public static ServiceRegistry services = null;
     public static ModuleDetails moduleDetails = null;
+
+    private static final Map<String, Map<String, String>> migrationMap;
+
+    static {
+        migrationMap = new HashMap<>();
+        Map<String, String> previous = new LinkedHashMap<>();
+        previous.put("3.1.0", "migrationThreeTwoOh");
+        previous.put("3.2.0", "migrationThreeTwoOh");
+        previous.put("3.3.0", "migrationThreeThreeOh");
+        migrationMap.put("3.3.0", previous);
+    }
 
     public static ServiceRegistry getServices() {
         return getServiceRegistry();
@@ -33,10 +49,37 @@ public class MigrationRunner {
         return services;
     }
 
+    @SuppressWarnings("unchecked")
     public static boolean checkMigration(ServiceRegistry services) {
         setServices(services);
         String previousVersion = getPreviousVersion();
         String currentVersion = getCurrentVersion();
+        if (isMigrationNeeded(previousVersion, currentVersion)) {
+            logger.error("Migration Needed!");
+            if (migrationMap.containsKey(currentVersion)) {
+                logger.error("Automigration path exists.");
+                Iterator it = migrationMap.get(currentVersion).entrySet().iterator();
+                while (it.hasNext()) {
+                    Map.Entry<String, String> pair = (Map.Entry) it.next();
+                    String migrationFor = pair.getKey();
+                    if (compareVersions(previousVersion, migrationFor) < 0) {
+                        logger.error("Update path found");
+                        String methodToRun = pair.getValue();
+                        logger.error("methodToRun: " + methodToRun);
+                        try {
+                            Method method = MigrationRunner.class.getMethod(methodToRun);
+                            logger.error("Invoking method: " + methodToRun);
+                            method.invoke(null);
+                        } catch (NoSuchMethodException nsme) {
+                            logger.error("No migration");
+                        } catch (IllegalAccessException | InvocationTargetException e) {
+                            logger.error("Error invoking migration", e);
+                        }
+                    }
+                    it.remove();
+                }
+            }
+        }
         return false;
     }
 
@@ -47,16 +90,9 @@ public class MigrationRunner {
         for (ModuleDetails module : modules) {
             if (module.getId().contains("mms-amp")) {
                 moduleDetails = module;
-                logger.error("MODULE VERSION IN DB: " + module.getModuleVersionNumber().toString());
-                List<String> editions = module.getEditions();
-                if (editions != null && editions.size() > 0) {
-                    for (String edition : editions) {
-                        logger.error("Edition: " + edition);
-                    }
-                } else {
-                    logger.error("No editions found.");
-                }
-                return module.getModuleVersionNumber().toString();
+                logger.error("MODULE VERSION IN DB: " + cleanVersion(module.getModuleVersionNumber().toString()));
+                return "3.2.4";
+                //return cleanVersion(module.getModuleVersionNumber().toString());
             }
         }
         return null;
@@ -65,12 +101,58 @@ public class MigrationRunner {
     public static String getCurrentVersion() {
         try {
             EmsConfig.setAlfrescoProperties(moduleDetails.getProperties());
-            logger.error("MODULE VERSION IN PROPERTIES: " + EmsConfig.get("module.version"));
-            return EmsConfig.get("module.version");
+            logger.error("MODULE VERSION IN PROPERTIES: " + cleanVersion(EmsConfig.get("module.version")));
+            return cleanVersion(EmsConfig.get("module.version"));
         } catch (Exception e) {
             logger.error(e);
         }
 
         return null;
+    }
+
+    public static String cleanVersion(String version) {
+        if (version == null) {
+            return null;
+        }
+        return version.indexOf('-') > -1 ? version.substring(0, version.indexOf('-')) : version;
+    }
+
+    public static int compareVersions(String previous, String current) {
+        String[] previousVersionString = cleanVersion(previous).split("\\.");
+        int[] previousVersionArr = new int[previousVersionString.length];
+        String[] currentVersionString = cleanVersion(current).split("\\.");
+        int[] currentVersionArr = new int[currentVersionString.length];
+
+        for (int i = 0; i < previousVersionString.length; i++) {
+            previousVersionArr[i] = Integer.valueOf(previousVersionString[i]);
+        }
+
+        for (int i = 0; i < currentVersionString.length; i++) {
+            currentVersionArr[i] = Integer.valueOf(currentVersionString[i]);
+        }
+
+        int max =
+            previousVersionArr.length > currentVersionArr.length ? previousVersionArr.length : currentVersionArr.length;
+
+        for (int i = 0; i < max; i++) {
+            int prev = i < previousVersionArr.length ? previousVersionArr[i] : 0;
+            int curr = i < currentVersionArr.length ? currentVersionArr[i] : 0;
+            if (prev != curr) {
+                return prev < curr ? -1 : 1;
+            }
+        }
+        return 0;
+    }
+
+    public static boolean isMigrationNeeded(String previousVersion, String currentVersion) {
+        return migrationMap.containsKey(cleanVersion(currentVersion)) && compareVersions(previousVersion, currentVersion) < 0;
+    }
+
+    public static void migrationThreeThreeOh() {
+        logger.error("RUNNING migrationThreeThreeOh");
+    }
+
+    public static void migrationThreeTwoOh() {
+        logger.error("RUNNING migrationThreeTwoOh");
     }
 }
