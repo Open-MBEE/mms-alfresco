4d482115ce061b8d975483764caf2b2d76b57492
Mann, Laura (393A-Affiliate) <Laura.Mann@jpl.nasa.gov>
2018-04-17 17:33:37 -0700

    fix for update

--- mms-ent/migration/3.3.0/update_commit_object.py
+++ mms-ent/migration/3.3.0/update_commit_object.py
@@ -49,42 +49,45 @@ def main(args):
     #     #scroll_id = iterate['_scroll_id']
     #     dupes = dupes + iterate_scroll(es, s_id)
 
-    first_page = es.search(
-        index='project-id_9_25_13_4_05_00_pm_52a679e5_1415678a941_56c_sscae_cmr_128_149_130_63',
-        doc_type='commit',
-        scroll='2m',
-        size=10000)
-    dupes = find_dupes(first_page.get('hits').get('hits'))
-    s_id = first_page['_scroll_id']
-        #scroll_size = first_page['hits']['total']
-        #iterate = es.scroll(scroll_id=s_id, scroll="2m")
-        #scroll_id = iterate['_scroll_id']
-    dupes = dupes + iterate_scroll(es, s_id)
-
-    # search_for_ids = es.get(
+    # first_page = es.search(
     #     index='project-id_9_25_13_4_05_00_pm_52a679e5_1415678a941_56c_sscae_cmr_128_149_130_63',
     #     doc_type='commit',
-    #     id='7d7e07f2-2a81-4b21-98c5-e1a7ccead848')
+    #     scroll='2m',
+    #     size=10000)
+    # dupes = find_dupes(first_page.get('hits').get('hits'))
+    # s_id = first_page['_scroll_id']
+    # dupes = dupes + iterate_scroll(es, s_id)
+    # print(dupes)
+
+    dupes = [('669964e1-9f47-4d90-8512-798b44cd1bd3', 'project-id_9_25_13_4_05_00_pm_52a679e5_1415678a941_56c_sscae_cmr_128_149_130_63', 'MMS_1519155626332_5bb79ad0-f61c-4210-9072-d94485061592', '118e64f3-c4af-4933-8f7e-c27efdb4d194')]
     #
-    # source = search_for_ids['_source']
-    # new_ids = find_correct(dupes)
-    #print(dupes)
-    #print(dupes)
     project_actions = []
     for d in dupes:
         new_added = find_correct_id(es, d)
+        print(json.dumps(new_added, indent=4, sort_keys=True))
+        print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
         search_for_ids = es.get(
             index=d[1],
             doc_type='commit',
             id=d[0])
-        added = search_for_ids['_source'].get('added')
+        added = search_for_ids['_source']['added']
+        print(json.dumps(search_for_ids, indent=4, sort_keys=True))
+        print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
         for entry in added:
             if entry['id'] == d[2] and entry['_elasticId'] == d[3]:
                 continue
             else:
                 new_added.append(entry)
-        project_actions.append(add_actions(new_added, d[0], 'commit'))
+        new_added.append({'id': entry['id'],'_elasticId':entry['_elasticId']})
+        print(json.dumps(new_added, indent=4, sort_keys=True))
+        print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+        project_actions.append(add_actions(new_added, d[0], 'commit', d[1]))
     helpers.bulk(es, project_actions)
+    search_for_ids = es.get(
+        index=d[1],
+        doc_type='commit',
+        id=d[0])
+    print(json.dumps(search_for_ids, indent=4, sort_keys=True))
 
 
 def iterate_scroll(es, scroll_id):
@@ -115,34 +118,6 @@ def find_dupes(hits):
                 projectId = hit['_source']['_projectId'].lower()
                 ids.append((id, projectId, entry['id'], entry['_elasticId']))
     return list(set([x for x in ids if ids.count(x) > 1]))
-
-def delete_duplicates(dupes):
-    return ' '
-#     POST test/_doc/1/_update
-# {
-#     "script" : {
-#         "source": "if (ctx._source.tags.contains(params.tag)) { ctx.op = 'delete' } else { ctx.op = 'none' }",
-#         "lang": "painless",
-#         "params" : {
-#             "tag" : "green"
-#         }
-#     }
-# }
-#int total = 0; for (int i = 0; i < doc['added'].length; ++i) { total += doc['goals'][i]; } return total;
-# POST test/_doc/1/_update
-# {
-#     "script" : "ctx._source.remove('new_field')"
-# }
-# GET project-id_9_25_13_4_05_00_pm_52a679e5_1415678a941_56c_sscae_cmr_128_149_130_63/commit/7d7e07f2-2a81-4b21-98c5-e1a7ccead848
-# {
-#     "script" : "def total = new ArrayList(); for (int i = 0; i < doc['added'].length; ++i) { total.add(i); } return total"
-# }
-#curl -X GET http://localhost:9200/project-id_9_25_13_4_05_00_pm_52a679e5_1415678a941_56c_sscae_cmr_128_149_130_63/commit/cbeb265b-9b73-4144-931b-de29c99155b4?pretty -d '{"script": {"inline": "def total = new ArrayList(); for (int i = 0; i < doc['added'].length; ++i) { total.add(i); } return total"}}'
-
-#curl -X GET http://localhost:9200/project-id_9_25_13_4_05_00_pm_52a679e5_1415678a941_56c_sscae_cmr_128_149_130_63/commit/cbeb265b-9b73-4144-931b-de29c99155b4/_source?_source_include=added -d '{"query": {"script": {"script": {"inline": "return doc['_source']"}}}}'
-
-#curl -XGET 'localhost:9200/twitter/tweet/1/_source?_source_include=*.id&_source_exclude=entities'&pretty'
-
 
 # def find_correct_ids(es, dupes):
 #     updates = []
@@ -180,25 +155,32 @@ def find_correct_id(es, d):
         index=d[1],
         doc_type='element',
         id=d[3])
+    #print(json.dumps(search_for_ids, indent=4, sort_keys=True))
+    #print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
     source = search_for_ids['_source']
     association_obj = es.search(
         index=d[1],
         doc_type='element',
         body= define_query(d[0], source.get('associationId')))
-    ass_obj = {'id': association_obj.get('_source').get('id'), '_elasticId': association_obj.get('_source').get('_elasticId')}
+    ass_obj = {'id': association_obj.get('hits').get('hits')[0].get('_source').get('id'), '_elasticId': association_obj.get('hits').get('hits')[0].get('_source').get('_elasticId')}
     update_added.append(ass_obj)
+    #print(json.dumps(association_obj, indent=4, sort_keys=True))
+    #print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
     appliedStereotypeInstanceId_obj = es.search(
         index=d[1],
         doc_type='element',
         body=define_query(d[0], source.get('appliedStereotypeInstanceId')))
-    app_obj = {'id': appliedStereotypeInstanceId_obj.get('_source').get('id') ,'_elasticId': appliedStereotypeInstanceId_obj.get('_source').get('_elasticId')}
+    # print(json.dumps(appliedStereotypeInstanceId_obj, indent=4, sort_keys=True))
+    app_obj = {'id': appliedStereotypeInstanceId_obj.get('hits').get('hits')[0].get('_source').get('id') ,'_elasticId': appliedStereotypeInstanceId_obj.get('hits').get('hits')[0].get('_source').get('_elasticId')}
     update_added.append(app_obj)
     ownedEndId_obj = es.search(
         index=d[1],
         doc_type='element',
-        body=define_query(d[0], appliedStereotypeInstanceId_obj.get('ownedEndIds')[0]))
-    owned_obj = {'id': ownedEndId_obj.get('_source').get('id'), '_elasticId': ownedEndId_obj.get('_source').get('_elasticId')}
+        body=define_query(d[0], association_obj.get('hits').get('hits')[0].get('_source').get('ownedEndIds')[0]))
+    owned_obj = {'id': ownedEndId_obj.get('hits').get('hits')[0].get('_source').get('id'), '_elasticId': ownedEndId_obj.get('hits').get('hits')[0].get('_source').get('_elasticId')}
     update_added.append(owned_obj)
+    # print(json.dumps(ownedEndId_obj, indent=4, sort_keys=True))
+    # print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
     return update_added
 
 def define_query(commitId, sysmlid):
@@ -206,36 +188,22 @@ def define_query(commitId, sysmlid):
         "query": {
             "bool": {
                 "must": [
-                    {
-                        "term": {
-                            "id": {
-                                "value": sysmlid,
-                            }
-                        }
-                    },
-                    {
-                        "term": {
-                            "_commitId": commitId
-                        }
-                    }
+                    {"term": {"id": sysmlid}},
+                    {"term": {"_commitId": commitId}}
                 ]
             }
         }
     }
 
-def add_actions(added, id, type):
+def add_actions(edit, id, type, index):
     update = {}
-    params = {}
-    script = {}
+    added = {}
     update['_op_type'] = 'update'
-    update['_index'] = 'mms'
+    update['_index'] = index
     update['_type'] = type
     update['_id'] = id
-    script['inline'] = "ctx._source.added = params.added"
-    params['added'] = added
-    script['params'] = params
-    update['script'] = script
-
+    added['added'] = edit
+    update['doc'] = added
     return update
 
 def get_projects(base_url, ticket):
@@ -256,3 +224,20 @@ if __name__ == '__main__':
     #     print "You need to pass the elastic host, mms host, and mms admin username"
     # else:
     #     main(sys.argv)
+
+
+# curl -XGET 'localhost:9200/_search?pretty' -H 'Content-Type: application/json' -d'
+# {
+#     "query": {
+#         "bool": {
+#             "must": [
+#                 {"term": {"id": "MMS_1519155626332_f7eecf60-7500-4986-9b04-0f3b896f96d4"}},
+#                 {"term": {"_commitId": "669964e1-9f47-4d90-8512-798b44cd1bd3"}}
+#             ]
+#         }
+#     }
+# }'
+
+# curl -X POST 'localhost:9200/project-id_9_25_13_4_05_00_pm_52a679e5_1415678a941_56c_sscae_cmr_128_149_130_63/commit/669964e1-9f47-4d90-8512-798b44cd1bd3/_update' -H 'Content-Type: application/json' -d '{"doc" : {"added" : []}}'
+
+# curl -X GET 'localhost:9200/project-id_9_25_13_4_05_00_pm_52a679e5_1415678a941_56c_sscae_cmr_128_149_130_63/commit/669964e1-9f47-4d90-8512-798b44cd1bd3'
