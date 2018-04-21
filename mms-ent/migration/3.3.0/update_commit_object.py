710f42321271159351c97efd9a0ee7e5b72a801d
Mann, Laura (393A-Affiliate) <Laura.Mann@jpl.nasa.gov>
2018-04-21 11:27:00 -0700

    functional for multi dup entries

--- mms-ent/migration/3.3.0/update_commit_object.py
+++ mms-ent/migration/3.3.0/update_commit_object.py
@@ -23,7 +23,7 @@ def main(args):
     # Get every dupe that isn't a intial commit
     dupes = {}
     for index in es.indices.get('*'):
-        print("We working on dis index  " + index)
+        #print("We working on dis index  " + index)
         first_page = es.search(
             index=index,
             doc_type='commit',
@@ -33,7 +33,7 @@ def main(args):
         s_id = first_page['_scroll_id']
         # print(str(type(iterate_scroll(es, s_id))) +"   "+str(len(iterate_scroll(es, s_id))))
         iterate_scroll(es, s_id, dupes)
-        print('Starting to remove dupes for project ' + index + ' There are this many dupes: ' + str(len(dupes)))
+        #print('Starting to remove dupes for project ' + index + ' There are this many dupes: ' + str(len(dupes)))
     print(str(len(dupes)) + " There are this many dupes in this org.")
     # print(dupes)
     # TODO: Count the actual length
@@ -46,33 +46,41 @@ def main(args):
     project_actions = []
     for commitId, dup in dupes.iteritems():
         new_added = find_correct_id(es, dup, commitId)
-        # if new_added is None:
-        #     commits_missing_info.append(commitId)
-        #     continue
-        # for d in dup:
-        #     try:
-        #         search_for_ids = es.get(
-        #             index=d[0],
-        #             doc_type='commit',
-        #             id=commitId)
-        #         added = search_for_ids['_source']['added']
-        #         # Add entries that are not dupes
-        #         for entry in added:
-        #             if entry['id'] == d[1] and entry['_elasticId'] == d[2]:
-        #                 continue
-        #             else:
-        #                 new_added.append(entry)
-        #         # add the dupe once
-        #         new_added.append({'id': d['id'], '_elasticId': d['_elasticId']})
-        #     except ElasticsearchException as e:
-        #         print("curl -X GET \'localhost:9200/"+d[0]+"/commit/"+commitId+"\'")
-    #     project_actions.append(add_actions(new_added, commitId, 'commit', dup[0]))
-    # helpers.bulk(es, project_actions)
-    # # if some commits were skipped print them
-    # if len(commits_missing_info) > 0:
-    #     print_errors(commits_missing_info)
-    # print('The number dupes after is :')
-    # print(len(check_all(es)))
+        if new_added is None:
+            commits_missing_info.append(commitId)
+            continue
+        for d in dup:
+            try:
+                search_for_ids = es.get(
+                    index=d[0],
+                    doc_type='commit',
+                    id=commitId)
+                added = search_for_ids['_source']['added']
+                # Add entries that are not dupes
+                for entry in added:
+                    if entry['id'] == d[1] and entry['_elasticId'] == d[2]:
+                        continue
+                    else:
+                        new_added.append(entry)
+                # add the dupe once
+                new_added.append({'id': d[1], '_elasticId': d[2]})
+                if commitId == 'cbeb265b-9b73-4144-931b-de29c99155b4':
+                    print(json.dumps(new_added, indent=4, sort_keys=True))
+                    print('that is the new object')
+                    print('this is the old object')
+                    print(json.dumps(added, indent=4, sort_keys=True))
+            except ElasticsearchException as e:
+                print("curl -X GET \'localhost:9200/"+d[0]+"/commit/"+commitId+"\'")
+            project_actions.append(add_actions(new_added, commitId, 'commit', dup[0]))
+        if len(project_actions) > 10000:
+            print('The number of updates was too large to update at once')
+            sys.exit(0)
+        #helpers.bulk(es, project_actions)
+        # if some commits were skipped print them
+        if len(commits_missing_info) > 0:
+            print_errors(commits_missing_info)
+        # print('The number dupes after is :')
+        # print(len(check_all(es)))
 
 
 def print_errors(commits_missing):
@@ -97,9 +105,8 @@ def find_dupes(hits):
     for hit in hits:
         id = hit['_id']
         added = hit['_source']['added']
-        if 'source' in hit['_source']:
-            if hit['_source']['source'].lower() == 'magicdraw':
-                continue
+        if 'source' in hit['_source'] and hit['_source']['source'].lower() == 'magicdraw':
+            continue
         elif len(added) > 5000:
             continue
         else:
@@ -122,10 +129,10 @@ def find_correct_id(es, d, commitId):
                 doc_type='element',
                 id=dup[2])
             source = search_for_ids['_source']
-            ass_obj, association_obj = get_association(es, d, source, commitId)
-            app_obj = get_stereotype(es, d, source, commitId)
+            ass_obj, association_obj = get_association(es, dup, source, commitId)
+            app_obj = get_stereotype(es, dup, source, commitId)
             if association_obj is not None:
-                owned_obj = get_owned_end(es, d, source, association_obj, commitId)
+                owned_obj = get_owned_end(es, dup, association_obj, commitId)
             else:
                 return None
             if ass_obj is not None and association_obj is not None:
@@ -142,47 +149,67 @@ def find_correct_id(es, d, commitId):
                 return None
         except ElasticsearchException as e:
             print(e)
-            print("curl -X GET \'localhost:9200/"+dup[0]+"/element/"+dup[2]+"\'")
+            print("curl -X GET \'localhost:9200/" + dup[0] + "/element/" + dup[2] + "\'")
             sys.exit(0)
     return update_added
 
 
 def get_association(es, d, source, commitId):
-    association_obj = es.search(
-        index=d[0],
-        doc_type='element',
-        body=define_query(commitId, source.get('associationId')))
-    if association_obj is not None and len(association_obj.get('hits').get('hits')) > 0:
-        return {'id': association_obj.get('hits').get('hits')[0].get('_source').get('id'),
-                '_elasticId': association_obj.get('hits').get('hits')[0].get('_source').get(
-                    '_elasticId')}, association_obj
-    else:
-        return None, None
+    try:
+        association_obj = es.search(
+            index=d[0],
+            doc_type='element',
+            body=define_query(commitId, source.get('associationId')))
+        if association_obj is not None and len(association_obj.get('hits').get('hits')) > 0:
+            return {'id': association_obj.get('hits').get('hits')[0].get('_source').get('id'),
+                    '_elasticId': association_obj.get('hits').get('hits')[0].get('_source').get(
+                        '_elasticId')}, association_obj
+        else:
+            return None, None
+    except ElasticsearchException as e:
+        print(e)
+        print('association')
+        print(d)
+        sys.exit(0)
 
 
 def get_stereotype(es, d, source, commitId):
-    appliedStereotypeInstanceId_obj = es.search(
-        index=d[0],
-        doc_type='element',
-        body=define_query(commitId, source.get('appliedStereotypeInstanceId')))
-    if appliedStereotypeInstanceId_obj is not None and len(appliedStereotypeInstanceId_obj.get('hits').get('hits')) > 0:
-        return {'id': appliedStereotypeInstanceId_obj.get('hits').get('hits')[0].get('_source').get('id'),
-                '_elasticId': appliedStereotypeInstanceId_obj.get('hits').get('hits')[0].get('_source').get(
-                    '_elasticId')}
-    else:
-        return None
+    try:
+        appliedStereotypeInstanceId_obj = es.search(
+            index=d[0],
+            doc_type='element',
+            body=define_query(commitId, source.get('appliedStereotypeInstanceId')))
+        if appliedStereotypeInstanceId_obj is not None and len(
+            appliedStereotypeInstanceId_obj.get('hits').get('hits')) > 0:
+            return {'id': appliedStereotypeInstanceId_obj.get('hits').get('hits')[0].get('_source').get('id'),
+                    '_elasticId': appliedStereotypeInstanceId_obj.get('hits').get('hits')[0].get('_source').get(
+                        '_elasticId')}
+        else:
+            return None
+    except ElasticsearchException as e:
+        print(e)
+        print('stereotype')
+        print(d)
+        sys.exit(0)
 
 
-def get_owned_end(es, d, source, association_obj, commitId):
-    ownedEndId_obj = es.search(
-        index=d[0],
-        doc_type='element',
-        body=define_query(commitId, association_obj.get('hits').get('hits')[0].get('_source').get('ownedEndIds')[0]))
-    if ownedEndId_obj is not None and len(ownedEndId_obj.get('hits').get('hits')) > 0:
-        return {'id': ownedEndId_obj.get('hits').get('hits')[0].get('_source').get('id'),
+def get_owned_end(es, d, association_obj, commitId):
+    try:
+        ownedEndId_obj = es.search(
+            index=d[0],
+            doc_type='element',
+            body=define_query(commitId,
+                              association_obj.get('hits').get('hits')[0].get('_source').get('ownedEndIds')[0]))
+        if ownedEndId_obj is not None and len(ownedEndId_obj.get('hits').get('hits')) > 0:
+            return {'id': ownedEndId_obj.get('hits').get('hits')[0].get('_source').get('id'),
                 '_elasticId': ownedEndId_obj.get('hits').get('hits')[0].get('_source').get('_elasticId')}
-    else:
-        return None
+        else:
+           return None
+    except ElasticsearchException as e:
+        print(e)
+        print('owned')
+        print(d)
+        sys.exit(0)
 
 
 def define_query(commitId, sysmlid):
@@ -240,11 +267,27 @@ def double_check(es, s_id):
     iterate = es.scroll(scroll_id=s_id, scroll="2m")
     s_id = iterate['_scroll_id']
     hits = iterate.get('hits').get('hits')
-    result = find_dupes(hits)
+    result = find_list_dup(hits)
     if hits:
-        return result.update(double_check(es, s_id))
+        return result + double_check(es, s_id)
     else:
         return result
+
+def find_list_dup(hits):
+    ids = []
+    for hit in hits:
+        id = hit['_id']
+        added = hit['_source']['added']
+        if 'source' in hit['_source']:
+            if hit['_source']['source'].lower() == 'magicdraw':
+                continue
+        elif len(added) > 5000:
+            continue
+        else:
+            for entry in added:
+                projectId = hit['_source']['_projectId'].lower()
+                ids.append((id, projectId, entry['id'], entry['_elasticId']))
+    return list(set([x for x in ids if ids.count(x) > 1]))
 
 
 if __name__ == '__main__':
@@ -255,30 +298,30 @@ if __name__ == '__main__':
     #     main(sys.argv)
 
 
-# curl -XGET 'localhost:9200/_search?pretty' -H 'Content-Type: application/json' -d'
-# {
-#     "query": {
-#         "bool": {
-#             "must": [
-#                 {"term": {"id": "blah"}},
-#                 {"term": {"_commitId": "blah"}}
-#             ]
-#         }
-#     }
-# }'
+    # curl -XGET 'localhost:9200/_search?pretty' -H 'Content-Type: application/json' -d'
+    # {
+    #     "query": {
+    #         "bool": {
+    #             "must": [
+    #                 {"term": {"id": "blah"}},
+    #                 {"term": {"_commitId": "blah"}}
+    #             ]
+    #         }
+    #     }
+    # }'
 
-# curl -X POST 'localhost:9200/project-id_9_25_13_4_05_00_pm_52a679e5_1415678a941_56c_sscae_cmr_128_149_130_63/commit/669964e1-9f47-4d90-8512-798b44cd1bd3/_update' -H 'Content-Type: application/json' -d '{"doc" : {"added" : []}}'
+    # curl -X POST 'localhost:9200/project-id_9_25_13_4_05_00_pm_52a679e5_1415678a941_56c_sscae_cmr_128_149_130_63/commit/669964e1-9f47-4d90-8512-798b44cd1bd3/_update' -H 'Content-Type: application/json' -d '{"doc" : {"added" : []}}'
 
-# curl -X GET 'localhost:9200/project-411c7459-4e09-4dda-b534-4b1fa7aaa140/commit/361169dc-25ec-416a-9cb8-d1fae420266f'
-# print(json.dumps(ownedEndId_obj, indent=4, sort_keys=True))
-# print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-# first_page = es.search(
-#     index='project-id_9_25_13_4_05_00_pm_52a679e5_1415678a941_56c_sscae_cmr_128_149_130_63',
-#     doc_type='commit',
-#     scroll='2m',
-#     size=10000)
-# dupes = find_dupes(first_page.get('hits').get('hits'))
-# s_id = first_page['_scroll_id']
-# dupes = dupes + iterate_scroll(es, s_id)
-# print(dupes)
-# dupes = [('669964e1-9f47-4d90-8512-798b44cd1bd3', 'project-id_9_25_13_4_05_00_pm_52a679e5_1415678a941_56c_sscae_cmr_128_149_130_63', 'MMS_1519155626332_5bb79ad0-f61c-4210-9072-d94485061592', '118e64f3-c4af-4933-8f7e-c27efdb4d194')]
+    # curl -X GET 'localhost:9200/project-411c7459-4e09-4dda-b534-4b1fa7aaa140/commit/361169dc-25ec-416a-9cb8-d1fae420266f'
+    # print(json.dumps(ownedEndId_obj, indent=4, sort_keys=True))
+    # print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    # first_page = es.search(
+    #     index='project-id_9_25_13_4_05_00_pm_52a679e5_1415678a941_56c_sscae_cmr_128_149_130_63',
+    #     doc_type='commit',
+    #     scroll='2m',
+    #     size=10000)
+    # dupes = find_dupes(first_page.get('hits').get('hits'))
+    # s_id = first_page['_scroll_id']
+    # dupes = dupes + iterate_scroll(es, s_id)
+    # print(dupes)
+    # dupes = [('669964e1-9f47-4d90-8512-798b44cd1bd3', 'project-id_9_25_13_4_05_00_pm_52a679e5_1415678a941_56c_sscae_cmr_128_149_130_63', 'MMS_1519155626332_5bb79ad0-f61c-4210-9072-d94485061592', '118e64f3-c4af-4933-8f7e-c27efdb4d194')]
