4eb05d39786cdd98de266c359e18fa7087af8ab2
Ward Schweitzer <ward.a.schweitzer@boeing.com>
2018-05-10 15:57:37 -0400

    Attempting to enforce some kind of limitation on the number of idle connections without sacrificing too much performance.

--- mms-ent/repo-amp/src/main/java/gov/nasa/jpl/view_repo/db/PostgresPool.java
+++ mms-ent/repo-amp/src/main/java/gov/nasa/jpl/view_repo/db/PostgresPool.java
@@ -4,62 +4,124 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.RemovalCause;
+import com.google.common.cache.RemovalNotification;
 import org.apache.commons.dbcp.BasicDataSource;
 
 import gov.nasa.jpl.view_repo.util.EmsConfig;
+import org.apache.commons.dbcp.DelegatingConnection;
+import org.apache.log4j.Logger;
 
 public class PostgresPool {
+    static Logger logger = Logger.getLogger(PostgresPool.class);
 
-    private static final int MAX_IDLE_CONN = 2;
-    private static final int MAX_ACTIVE_CONN = 96;
+    static final int MAX_IDLE_CONN = 2;
+    static final int MIN_IDLE_CONN = 0;
+    static final int MAX_ACTIVE_CONN = 96;
+    static final int MAX_CONN_LIMIT = 192;
 
-    private static final String PG_CONN_MAX = "pg.conn.max";
-    private static final String PG_USER = "pg.user";
-    private static final String PG_PASS = "pg.pass";
+    static final String PG_CONN_MAX = "pg.conn.max";
+    static final String PG_USER = "pg.user";
+    static final String PG_PASS = "pg.pass";
+    static final String PG_SEC = "pg.secured";
 
+    private String host;
+    private String name;
     private String connectString;
-    private static Map<String, PostgresPool> dataSource = new HashMap<>();
-    private Map<String, BasicDataSource> bds = new HashMap<>();
+    private BasicDataSource bds = null;
 
-    private PostgresPool(String connectString) {
-        this.connectString = connectString;
-        if (!bds.containsKey(connectString)) {
-            bds.put(connectString, new BasicDataSource());
-            bds.get(connectString).setDriverClassName("org.postgresql.Driver");
-            bds.get(connectString).setUrl(connectString);
-            bds.get(connectString).setUsername(EmsConfig.get(PG_USER));
-            bds.get(connectString).setPassword(EmsConfig.get(PG_PASS));
-            bds.get(connectString).setInitialSize(10);
-            bds.get(connectString).setMaxIdle(MAX_IDLE_CONN);
-            bds.get(connectString).setMaxActive((!EmsConfig.get(PG_CONN_MAX).equals("")) ? Integer.parseInt(EmsConfig.get(
-                PG_CONN_MAX)) : MAX_ACTIVE_CONN);
-            bds.get(connectString).setMaxWait(10000);
-            bds.get(connectString).setDefaultAutoCommit(true);
-            bds.get(connectString).setRemoveAbandonedTimeout(1);
-            bds.get(connectString).setLogAbandoned(true);
-            bds.get(connectString).setRemoveAbandoned(true);
+    private static Map<String, PostgresPool> dataSources = new HashMap<>();
+    private static Map<String, Cache<String, PostgresPool>> activeDataSources = new HashMap<>();
+
+    interface IBasicDataSourceFactory {
+        BasicDataSource getNewBasicDataSource();
+    }
+
+    private static IBasicDataSourceFactory basicDataSourceFactory = null;
+
+    private PostgresPool(String host, String name) {
+        this.host = host;
+        this.name = name;
+        this.connectString = getConnectString(host, name);
+
+        this.bds = getBasicDataSourceFactory().getNewBasicDataSource();
+        this.bds.setDriverClassName("org.postgresql.Driver");
+        this.bds.setUrl(this.connectString);
+        this.bds.setUsername(EmsConfig.get(PG_USER));
+        this.bds.setPassword(EmsConfig.get(PG_PASS));
+        this.bds.setInitialSize(10);
+        this.bds.setMaxIdle(MAX_IDLE_CONN);
+        this.bds.setMinIdle(MIN_IDLE_CONN);
+        this.bds.setMaxActive((!EmsConfig.get(PG_CONN_MAX).equals("")) ? Integer.parseInt(EmsConfig.get(
+            PG_CONN_MAX)) : MAX_ACTIVE_CONN);
+        this.bds.setMaxWait(10000);
+        this.bds.setDefaultAutoCommit(true);
+        this.bds.setRemoveAbandonedTimeout(1);
+        this.bds.setLogAbandoned(true);
+        this.bds.setRemoveAbandoned(true);
+        this.bds.setTimeBetweenEvictionRunsMillis(1000*60);
+        this.bds.setMinEvictableIdleTimeMillis(1000*60*5);
+        if(EmsConfig.get(PG_SEC).toLowerCase().equals("true")){
+            this.bds.setConnectionProperties("ssl=true");
         }
     }
 
-    public static PostgresPool getInstance(String host, String name) {
-        String connectString = host + name;
-        if (!dataSource.containsKey(connectString)) {
-            dataSource.put(connectString, newInstance(host, name));
+    private static String getConnectString(String host, String name) {
+        return host + name;
+    }
+
+    public static synchronized PostgresPool getInstance(String host, String name) {
+        String connectString = getConnectString(host, name);
+        Cache<String, PostgresPool> cache = getActiveDatasetCache(host);
+        PostgresPool pool = cache.getIfPresent(connectString);
+        if(pool == null){
+            pool = dataSources.get(connectString);
+            if (pool == null) {
+                pool = newInstance(host, name);
+                dataSources.put(connectString, pool);
+            }
         }
-        return dataSource.get(connectString);
+        return pool;
     }
 
     private static PostgresPool newInstance(String host, String name) {
-        return new PostgresPool(host + name);
+        return new PostgresPool(host, name);
     }
 
     public Connection getConnection() throws SQLException {
-        return this.bds.get(connectString).getConnection();
+        goActive();
+        Connection connection = this.bds.getConnection();
+        updatePool();
+        return connection;
+    }
+
+    private void updatePool() {
+        getActiveDatasetCache(host).put(this.connectString, this);
+    }
+
+    private int getWeight() {
+        return this.bds.getNumActive() + this.bds.getNumIdle();
+    }
+
+    private void goActive() {
+        this.bds.setMaxIdle(MAX_IDLE_CONN);
+    }
+
+    private void goIdle() {
+        this.bds.setMaxIdle(MIN_IDLE_CONN);
+        if(this.bds.getNumActive() > 0){
+            logger.warn("Database connection pool (" +
+                getConnectString(host, name) +
+                ") with active connections was instructed to go idle.  Database may be overloaded");
+        }
     }
 
     public static Connection getStandaloneConnection(String host, String name) throws SQLException {
-        BasicDataSource bds = new BasicDataSource();
+        BasicDataSource bds = getBasicDataSourceFactory().getNewBasicDataSource();
         bds.setDriverClassName("org.postgresql.Driver");
         bds.setUrl(host + name);
         bds.setUsername(EmsConfig.get(PG_USER));
@@ -81,13 +143,52 @@ public class PostgresPool {
      */
     static void removeConnection(String host, String name) {
         String connectString;
+        //TODO: Why is this different here??
         if (!name.startsWith("_")) {
             connectString = host + "_" + name;
         } else {
             connectString = host + name;
         }
-        if (dataSource.containsKey(connectString)) {
-            dataSource.remove(connectString);
-        }
+        dataSources.remove(connectString);
+        getActiveDatasetCache(host).invalidate(connectString);
     }
+
+    private static Cache<String, PostgresPool> getActiveDatasetCache(String host){
+        Cache<String, PostgresPool> cache = activeDataSources.get((host));
+
+        if(cache == null) {
+            cache = CacheBuilder.newBuilder()
+                .maximumWeight(MAX_CONN_LIMIT)
+                .weigher((String key, PostgresPool pool) -> pool.getWeight())
+                .expireAfterAccess(5, TimeUnit.MINUTES)
+                .removalListener((RemovalNotification<String, PostgresPool> v) -> {
+                    if (v.getCause() != RemovalCause.REPLACED)
+                        v.getValue().goIdle();
+                })
+                .build();
+            activeDataSources.put(host, cache);
+        }
+        return cache;
+    }
+
+
+    public static IBasicDataSourceFactory getBasicDataSourceFactory() {
+        if(basicDataSourceFactory == null)
+            basicDataSourceFactory = () -> new BasicDataSource();
+        return basicDataSourceFactory;
+    }
+
+    public static void setBasicDataSourceFactory(IBasicDataSourceFactory basicDataSourceFactory) {
+        PostgresPool.basicDataSourceFactory = basicDataSourceFactory;
+    }
+
+    static void purgeDatabasePools(){
+        activeDataSources.clear();
+        dataSources.clear();
+    }
+    static void idleDatabasePools(){
+        for(Cache<String, PostgresPool> cache : activeDataSources.values())
+            cache.invalidateAll();
+    }
+
 }
