1a41b63694a262d18083cb9706d1ace63b2f49b6
Jason Han <jason.han@jpl.nasa.gov>
2018-08-30 15:42:26 -0700

    Updating circleci config

--- mms-ent/build.sh
+++ mms-ent/build.sh
@@ -7,5 +7,6 @@ if [ "$1" == "" ]; then
     MAVEN_OPTS="-Xms256m -Xmx8G -Xdebug" ./mvnw clean package -f pom.xml -P build -Ddependency.surf.version=6.3 -Dmaven.test.skip=true -U -Dmaven.repo.local=$HOME/.m2/repository;
 else
     ./mvnw versions:set -DnewVersion=$1 -DgenerateBackupPoms=false -Ddependency.surf.version=6.3
+    rm -f ../.circleci/config.yml
+    sed "s/\${version}/$1/" ../.circleci/config.yml.template > ../.circleci/config.yml
 fi
-
